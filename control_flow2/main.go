a2444d54c499c1d8792e0681c6346f5d9311a933
halacrcrc <2253453812q@gmail.com>
2023-01-10 21:18:32 +0800

    control_flow2

---
+++ control_flow2/main.go
@@ -0,0 +1,18 @@
+﻿package main
+
+import "fmt"
+
+var sroot, x float32 = 1, 0
+
+func main() {
+	fmt.Scanln(&x)
+	for i := 0; i < 10; i++ {
+		temp := sroot
+		sroot = sroot - (sroot*sroot-x)/(2*sroot)
+		if temp == sroot {
+			fmt.Print("Square root is: ", sroot)
+			break
+		}
+		fmt.Println("A guess for square root is ", sroot)
+	}
+}
