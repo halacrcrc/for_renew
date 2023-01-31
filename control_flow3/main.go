0dfc24a4a19325c7d73258b3973815f92b9b6f93
halacrcrc <2253453812q@gmail.com>
2023-01-31 10:30:00 +0800

    control_flow3

---
+++ control_flow3/main.go
@@ -0,0 +1,21 @@
+﻿package main
+
+import "fmt"
+
+func main() {
+	val := 0
+	for {
+		fmt.Println("plz enter number: ")
+		fmt.Scanf("%d", &val)
+		switch {
+		case val < 0:
+			panic("Panic!")
+		case val == 0:
+			{
+				fmt.Println("0 is neither negative nor positive")
+			}
+		default:
+			fmt.Println("You entered: ", val)
+		}
+	}
+}
