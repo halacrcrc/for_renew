a72e329a48bf92b2311ee561783471b355850ac4
halacrcrc <2253453812q@gmail.com>
2023-11-21 13:49:56 +0800

    practise

---
+++ practise/main.go
@@ -0,0 +1,38 @@
+﻿package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func checkAPI(api string, ch chan string) {
+	_, err := http.Get(api)
+	if err != nil {
+		ch <- fmt.Sprintf("ERROR: %s is down!\n", api)
+		return
+	}
+
+	ch <- fmt.Sprintf("SUCCESS: %s is up and running!\n", api)
+}
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	apis := []string{
+		"https://management.azure.com",
+		"https://dev.azure.com",
+		"https://api.github.com",
+		"https://outlook.office.com/",
+		"https://api.somewhereintheinternet.com/",
+		"https://graph.microsoft.com",
+	}
+
+	for _, api := range apis {
+		go checkAPI(api, ch)
+	}
+
+	elapsed := time.Since(start)
+	fmt.Print(<-ch)
+	fmt.Printf("Done! It took %v seconds!\n", elapsed.Seconds())
+}
